412a15b15b892af60e9b47bec1ce4d309564cfb7
Ryan Augustyn <jraugustyn07@gmail.com>
2023-02-17 11:18:46 -0600

    added battle logic

--- battlefield.py
+++ battlefield.py
@@ -1,14 +1,36 @@
+from random import range
+
+from robot import Robot
+from dinosaur import Dinosaur
+
 class Battlefield:
     def __init__(self):
-        self.robot = ''
-        self.dinosaur = ''
+        self.robot = Robot("MegaWeapon", "Robot Nunchucks")
+        self.dinosaur = Dinosaur("Ted", 5)
     
     def run_game(self):
         pass
     def display_welcome(self):
-        pass
+        print("Welcome to Robot vs Dinosaur\n\n")
     def battle_phase(self):
-        pass
+        #choose who attacks first
+        random = range(1,2)
+        if random == 1:
+            print("Robot strikes first!")
+            while self.robot.health > 0 and self.dinosaur.health > 0:
+                self.robot.attack()
+                self.dinosaur.attack()
+        if random == 2:
+            print("Dinosaur strikes first!")
+            while self.robot.health > 0 and self.dinosaur.health > 0:
+                self.dinosaur.attack()
+                self.robot.attack()
+
     def display_winner(self):
-        pass
+        if self.robot.health > 0 and self.dinosaur.health <= 0:
+            print(f"{self.robot.name} has won the battle!")
+        elif self.robot.health <= 0 and self.dinosaur.health > 0:
+            print(f"{self.dinosaur.name} has won the battle!")
+        else:
+            print("They have both perished! Oh the monstermainity!")
     
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from battlefield import Battlefield
 battle1 = Battlefield()
 
 
-'''(5 points): As a developer, I want to make at least 7 commits  2/7
+'''(5 points): As a developer, I want to make at least 7 commits  3/7
  
   
 (10 points): As a developer, I want the battle to conclude once either the Robot
